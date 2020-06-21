393f3a44cab63d4903653fa2136bfc06f76ee334
Drew Mitchell <hi@callmedrew.com>
2020-06-21 18:42:58 -0400

    Add function and tests for rolling the dice.

--- src/dice.rs
+++ src/dice.rs
@@ -1,5 +1,7 @@
+use rand::Rng;
 use regex::Regex;
 use crate::dice::DiceTerm::{Constant, Dice};
+use std::convert::TryInto;
 
 #[derive(Debug)]
 pub enum DiceTerm {
@@ -41,6 +43,19 @@ impl DiceTerm {
             .collect()
     }
 
+    pub fn roll(&self) -> i32 {
+        match self {
+            Dice{count, sides} => {
+                let max_value = (sides+1).try_into().unwrap();
+                let total: i32 = (0..(*count).abs())
+                    .map(|_| rand::thread_rng().gen_range(1, max_value))
+                    .sum();
+                total * sign(*count)
+            },
+            Constant(constant) => *constant
+        }
+    }
+
     /// Returns the expected average value of this term.
     pub fn average(&self) -> f64 {
         match self {
@@ -50,9 +65,40 @@ impl DiceTerm {
     }
 }
 
+/// Returns `+1`, `-1`, or `0` according to the sign of the input.
+fn sign(x: i32) -> i32 {
+    if x == 0 {
+        0
+    } else if x > 0 {
+        1
+    } else {
+        -1
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_dice_rolls() {
+        let roll = Dice{count: 1, sides: 20}.roll();
+        assert!(roll <= 20, "Roll {} should be no more than 20.", roll);
+        assert!(roll >= 1, "Roll {} should be at least 1.", roll);
+    }
+
+    #[test]
+    fn test_negative_dice_rolls() {
+        let roll = Dice{count: -3, sides: 6}.roll();
+        assert!(roll >= -18, "Roll {} should be at least -18.", roll);
+        assert!(roll <= -3, "Roll {} should be no more than -3.", roll);
+    }
+
+    #[test]
+    fn test_constant_rolls() {
+        assert_eq!(10, Constant(10).roll());
+        assert_eq!(-2, Constant(-2).roll());
+    }
 
     #[test]
     fn test_dice_averages() {
